45d1523cacdf7eaf0163c963a33117cf1648060f
ByNotDying <67306215+ByNotDying@users.noreply.github.com>
2023-06-03 20:32:18 -0700

    Bootstrap @import to add entire css
    Screw it, just copy the entire css from Bootstrap for now

--- app/characters/page.js
+++ app/characters/page.js
@@ -1,15 +1,29 @@
 
-
+import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
+    
     <main>
       <div>
         <p>
           Get started by editing;
-          
+          <button type="button" className="btn btn-primary btn-lg">Large button</button>
+    <button type="button" className="btn btn-secondary btn-lg">Large button</button>
         </p>
+        <button type="button" className="btn btn-primary btn-lg">Large button</button>
+<button type="button" className="btn btn-secondary btn-lg">Large button</button>
        </div>
+       <button type="button" className="btn btn-primary">Primary</button>
+<button type="button" className="btn btn-secondary">Secondary</button>
+<button type="button" className="btn btn-success">Success</button>
+<button type="button" className="btn btn-danger">Danger</button>
+<button type="button" className="btn btn-warning">Warning</button>
+<button type="button" className="btn btn-info">Info</button>
+<button type="button" className="btn btn-light">Light</button>
+<button type="button" className="btn btn-dark">Dark</button>
+
     </main>
   )
 }
